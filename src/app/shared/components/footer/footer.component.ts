073830d296c43efc31ebbf38a3a7978abc7d90fb
Marcos Dinei <marcos.fontes@parnamirim.rn.gov.br>
2023-03-14 12:45:02 -0300

    feat: footer (prototype)

---
+++ src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+  standalone: true,
+  imports: [CommonModule, MatToolbarModule]
+})
+export class FooterComponent {
+
+}
