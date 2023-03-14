2f356390b9397e6a562dd4228296f8051d56d558
Marcos Dinei <marcos.fontes@parnamirim.rn.gov.br>
2023-03-14 12:50:04 -0300

    feat: products (prototype)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './home/products/products.component';
+import { PdvComponent } from './pdv/pdv.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'produtos',
+        component: ProductsComponent
+      }
+    ]
+  },
+  {
+    path: 'pdv',
+    component: PdvComponent
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: '/'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
